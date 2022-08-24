c94ac401e1f70486ab1e3a825cb5aa8b1bd4993d
Юрий Аверьянов <averianovj@gmail.com>
2022-08-24 21:15:32 +0300

    first commit

---
+++ src/main/java/task/Randoms.java
@@ -0,0 +1,44 @@
+package task;
+
+import java.util.*;
+import java.util.function.Consumer;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+
+    private int min;
+    private int max;
+    public Randoms(int min, int max) {
+        this.min = min;
+        this.max = max;
+        random = new Random();
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return random.nextInt(max - min + 1) + min;
+            }
+        };
+    }
+
+    @Override
+    public void forEach(Consumer<? super Integer> action) {
+        Iterable.super.forEach(action);
+    }
+
+    @Override
+    public Spliterator<Integer> spliterator() {
+
+        return Iterable.super.spliterator();
+    }
+
+
+}
